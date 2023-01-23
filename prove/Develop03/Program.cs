b89737fa92e09d80e4073138c3fa3220d7c8b062
orpheen <nge21003@byui.edu>
2023-01-22 18:17:33 -0700

    add to prove

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptureReference = new Reference("Ether", "12", "26");
+        Scripture scripture = new Scripture(scriptureReference, "and when I had said this, the Lord spake unto me, saying: Fools mock, but they shall mourn; and my grace is sufficient for the meek, that they shall take no advantage of your weakness;");
+        ScriptureMemorizer scriptureMemorizer = new ScriptureMemorizer(scripture);
+
+        string userInput = "";
+
+        while (userInput != "quit" && scriptureMemorizer.hasWordsLeft() == true)
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+            Console.WriteLine();
+            Console.ReadLine();
+            scriptureMemorizer.removeWordsFromText();
+        }
     }
 }
